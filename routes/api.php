9f403121a5b9edb8ed0b084281a958853871b42d
daffiaffan <daffiaffan@student.telkomuniversity.ac.id>
2025-07-19 23:01:36 +0700

    feat :  login and seeder user ! "see the route api"

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,20 @@
 use App\Models\Participant;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AttendanceController;
 use App\Http\Controllers\ParticipantController;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
+Route::post('/inipunyaklaten', [AuthController::class, 'login'])->name('login');
+
+Route::middleware('auth:api')->group(
+    function () {
+        Route::get('/me', [AuthController::class, 'me']);
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::post('/refresh', [AuthController::class, 'refresh']);
+    }
+);
+
 
 Route::post('/scan', [AttendanceController::class, 'scan']);
 Route::apiResource('participants', ParticipantController::class);
