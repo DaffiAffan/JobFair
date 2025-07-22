93ed11ad5c5d94e725f7e9fc3f9f3b0235f08d9c
daffiaffan <daffiaffan@student.telkomuniversity.ac.id>
2025-07-22 13:46:08 +0700

    refactor : edit and delete route

--- routes/api.php
+++ routes/api.php
@@ -16,9 +16,9 @@ Route::middleware('auth:api')->group(
         Route::post('/refresh', [AuthController::class, 'refresh']);
 
         Route::get('/participants', [ParticipantController::class, 'index']);
-        Route::get('/participants/{id}', [ParticipantController::class, 'show']);
-        Route::put('/participants/{id}', [ParticipantController::class, 'update']);
-        Route::delete('/participants/{id}', [ParticipantController::class, 'destroy']);
+        Route::get('/participants/{participant}', [ParticipantController::class, 'show']);
+        Route::put('/participants/{participant}', [ParticipantController::class, 'update']);
+        Route::delete('/participants/{participant}', [ParticipantController::class, 'destroy']);
 
         Route::get('/attendances', [AttendanceController::class, 'index']);
         Route::post('/scan', [AttendanceController::class, 'scan']);
